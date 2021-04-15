ff6ed55784c12d91cc445134d4e368da34199a30
Joshua Schrader <schrader.joshua@icloud.com>
2021-04-15 17:28:31 +0200

    added ipv6 support

--- pypit.py
+++ pypit.py
@@ -6,6 +6,7 @@ import time
 from _thread import *
 from datetime import datetime as dt
 
+
 def spam(client_socket, address):
     c_time = time.time()  # get starting time
 
@@ -15,16 +16,16 @@ def spam(client_socket, address):
             client_socket.send("fail".encode())  # test if client is still connected
 
     except socket.error:
-        logging.info(f"{dt.now().strftime('%H:%M:%S')} [\033[1;31m-\033[0;0m] {address[0]} connection closed after {round(time.time() - c_time)} seconds.")
+        logging.info(f"{dt.now().strftime('%H:%M:%S')} [\033[1;31m-\033[0;0m] {address} connection closed after {round(time.time() - c_time)} seconds.")
 
 
 def main():
     logging.basicConfig(level=logging.DEBUG, filename="/var/log/pypit.log", filemode="w", format='%(message)s')
 
-    SERVER_HOST = "0.0.0.0"
+    SERVER_HOST = "::"
     SERVER_PORT = 22
 
-    s = socket.socket()
+    s = socket.socket(socket.AF_INET6)
     s.bind((SERVER_HOST, SERVER_PORT))
 
     s.listen()
@@ -32,7 +33,11 @@ def main():
 
     while True:
         client_socket, address = s.accept()
-        logging.info(f"{dt.now().strftime('%H:%M:%S')} [\033[1;32m+\033[0;0m] {address[0]} is connected.")
+        address = address[0]
+        if address.startswith("::ffff"):
+            address = address[7:]
+
+        logging.info(f"{dt.now().strftime('%H:%M:%S')} [\033[1;32m+\033[0;0m] {address} is connected.")
         start_new_thread(spam, (client_socket, address, ))
 
 
